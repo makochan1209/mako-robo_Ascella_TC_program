136742d6c4fd1420ab8907aefbc416a774af5e03
Makoto Yamashita <makochan12.9@gmail.com>
2023-07-01 20:46:01 +0900

    ある程度情報追加

--- TC_main.py
+++ TC_main.py
@@ -19,8 +19,12 @@ RANDOM_STATUS_MODE = True
 GRID_WIDTH = 40
 GRID_HEIGHT = 10
 
-pos=[0xf1, 0xf2, 0xf3, 0xf4, 0xf5, 0xf6] # ロボットの位置（通信そのまま）
-act=[0xf1, 0xf2, 0xf3, 0xf4, 0xf5, 0xf6] # ロボットの作業内容（通信そのまま）
+pos = [0xf1, 0xf2, 0xf3, 0xf4, 0xf5, 0xf6] # ロボットの位置（通信そのまま）
+destPos = [0xf1, 0xf2, 0xf3, 0xf4, 0xf5, 0xf6] # ロボットの行き先（通信そのまま）
+act = [0xf1, 0xf2, 0xf3, 0xf4, 0xf5, 0xf6] # ロボットの作業内容（通信そのまま）
+recvCom = [0xf1, 0xf2, 0xf3, 0xf4, 0xf5, 0xf6] # ロボットの受信通信内容（通信そのまま）
+transCom = [0xf1, 0xf2, 0xf3, 0xf4, 0xf5, 0xf6] # ロボットの送信通信内容（通信そのまま）
+connectStatus = [False, False, False, False, False, False]  # 接続できているか
 
 serBuffStr = []
 # 管制
@@ -63,11 +67,40 @@ def windowDaemon():
     for i in range(6):
         actTextBuf = ""
         if (act[i] == 0x00):
+            actTextBuf = "待機中"
+        elif (act[i] == 0x01):
+            actTextBuf = "走行中"
+        elif (act[i] == 0x02):
             actTextBuf = "探索中"
         else:
-            actTextBuf = "待機中"
+            actTextBuf = "不明"
 
-        configureTextBuf = str(i + 1) + "号機\n\n" + "場所: " + str(pos[i]) + "\n作業: " + actTextBuf
+        if (recvCom[i] == 0x00):
+            recvCommandBuf = "探索結果報告："
+        elif (recvCom[i] == 0x01):
+            recvCommandBuf = "位置到達報告："
+        elif (recvCom[i] == 0x02):
+            recvCommandBuf = "ボールシュート報告"
+        elif (recvCom[i] == 0x20):
+            recvCommandBuf = "行動指示要求"
+        elif (recvCom[i] == 0x21):
+            recvCommandBuf = "許可要求"
+        elif (recvCom[i] == 0x30):
+            recvCommandBuf = "通信可否報告"
+        else:
+            recvCommandBuf = "なし"
+
+        if (transCom[i] == 0x50):
+            transCommandBuf = "移動許可：" + destPos[i]
+        elif (transCom[i] == 0x51):
+            transCommandBuf = "行動許可："
+        else:
+            transCommandBuf = "なし"
+
+        if connectStatus[i]:
+            configureTextBuf = str(i + 1) + "号機\n\n" + "接続状態: " + "接続済\n\n" + "場所: " + str(pos[i]) + "\n状態: " + actTextBuf + "\n最終通信内容（受信）: " + recvCommandBuf + "\n最終通信内容（送信）: " + transCommandBuf
+        else:
+            configureTextBuf = str(i + 1) + "号機\n\n" + "接続状態: " + "未接続"
 
         if (i == 0):
             labelR1.configure(text=configureTextBuf)
@@ -84,14 +117,31 @@ def windowDaemon():
 
     mainWindow.after(50, windowDaemon)
 
-# 常時実行し通信・ウィンドウを更新する関数を実行するための最初の関数
+# ロボット本体との接続
 def connect():
-    buttonConnect.pack_forget()
+    buttonConnect.grid_forget()
+    for i in range(6):
+        connectStatus[i] = True
+    buttonStart.grid(row=5,column=0,columnspan=2)
 
 def exitTCApp():
     if SERIAL_MODE:
         ser.close()
     mainWindow.destroy()
+
+def keyPress(event):
+    # Enterのとき
+    if event.keycode == 13:
+        compStart()
+    # Numkey 1のとき
+    elif event.keycode == 49:
+        connect()
+    # Numkey 2のとき
+    elif event.keycode == 50:
+        compEmgStop()
+    # Numkey 9のとき
+    elif event.keycode == 57:
+        exitTCApp()
 
 # 以下メインルーチン
 
@@ -136,14 +186,16 @@ labelR5.grid(row=4,column=0)
 labelR6 = tk.Label(mainWindow, text='6号機', anchor=tk.N, width=GRID_WIDTH, height=GRID_HEIGHT)
 labelR6.grid(row=4,column=1)
 
-buttonConnect = tk.Button(mainWindow, text = "通信接続", command = connect)
+buttonConnect = tk.Button(mainWindow, text = "通信接続 (Num 1)", command = connect)
 buttonConnect.grid(row=5,column=0,columnspan=2)
 
-buttonStart = tk.Button(mainWindow, text = "競技開始", command = compStart)
-buttonEmgStop = tk.Button(mainWindow, text = "全ロボット緊急停止", command = compEmgStop)
+buttonStart = tk.Button(mainWindow, text = "競技開始 (Enter)", command = compStart)
+buttonEmgStop = tk.Button(mainWindow, text = "全ロボット緊急停止 (Num 2)", command = compEmgStop)
 
-buttonExit = tk.Button(mainWindow, text = "プログラム終了", command = exitTCApp)
+buttonExit = tk.Button(mainWindow, text = "プログラム終了 (Num 9)", command = exitTCApp)
 buttonExit.grid(row=10,column=0,columnspan=2)
+
+mainWindow.bind("<KeyPress>", keyPress)
 
 threadWindow = threading.Thread(target=windowDaemon, daemon=True)
 threadTC = threading.Thread(target=TCDaemon, daemon=True)
